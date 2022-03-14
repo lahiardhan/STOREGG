88ad4c0490ddda7975298d38e1ca60b2c8e73c3e
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-03-14 13:20:34 +0700

    [Update GameItem] mengganti elemen Image dari next dengan tag img html biasa dan menghapus layout='fixed' untuk memperbaiki tampilan mobile

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export interface GameItemProps {
@@ -14,19 +13,19 @@ export default function GameItem(props: GameItemProps) {
          <Link href={`/detail/${id}`}>
             <a>
                <div className="blur-sharp">
-                  <Image
+                  <img
                      className="thumbnail"
                      src={thumbnail}
                      width={205}
                      height={270}
                      alt="thumbnail"
-                     layout="fixed"
+                     
                   />
                </div>
                <div className="cover position-absolute bottom-0 m-32">
                   <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
                      <div className="game-icon mx-auto">
-                        <Image src="/icon/console.svg" width={54} height={36} alt="console" layout="fixed"/>
+                        <img src="/icon/console.svg" width={54} height={36} alt="console" />
                      </div>
                      <div>
                         <p className="fw-semibold text-white text-xl m-0">
