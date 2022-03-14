0243161627a4aa583972c0a755d1a29c0066a3d2
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-03-14 12:59:45 +0700

    Update layout menambahkan layout='fixed' untuk memperbaiki responsive di mobile

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -20,6 +20,7 @@ export default function GameItem(props: GameItemProps) {
                      width={205}
                      height={270}
                      alt="thumbnail"
+                     layout="fixed"
                   />
                </div>
                <div className="cover position-absolute bottom-0 m-32">
